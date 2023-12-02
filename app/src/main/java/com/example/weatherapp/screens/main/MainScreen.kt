e060f9d5b46bdbe9a5e64df2b93004b1e5cdcf5a
Aryaman Mishra <56508277+aryaman0098@users.noreply.github.com>
2023-12-02 15:44:31 +0530

    initial setup, added dependencies, created nav controller and splash screen
    I

---
+++ app/src/main/java/com/example/weatherapp/screens/main/MainScreen.kt
@@ -0,0 +1,12 @@
+package com.example.weatherapp.screens.main
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun MainScreen(
+    navController: NavController
+) {
+    Text(text = "MainScreen")
+}
